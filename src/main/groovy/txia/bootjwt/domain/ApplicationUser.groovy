3913951f0959e3dd8ebb052b8eb01c05ce467da9
Tracy Xia <txiasummer@gmail.com>
2017-09-20 11:46:54 -0400

    modified README

--- src/main/groovy/txia/bootjwt/domain/ApplicationUser.groovy
+++ src/main/groovy/txia/bootjwt/domain/ApplicationUser.groovy
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
+//I would have liked to call this simply "User", but Spring Security framework has a User class... not worth the effort to deal with the confusion
 @Entity
 class ApplicationUser {
     @Id
